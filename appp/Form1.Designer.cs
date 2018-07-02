04a289783c1f4e754ebb1351cec497d0087fd455
公司电脑 <sunyiming537@126.com>
2018-07-02 11:35:07 +0800

    升级为双串口转换

--- appp/Form1.Designer.cs
+++ appp/Form1.Designer.cs
@@ -33,10 +33,19 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.textBoxCom = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.serialPort2 = new System.IO.Ports.SerialPort(this.components);
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // serialPort1
@@ -49,18 +58,20 @@
             // textBox2
             // 
             this.textBox2.Font = new System.Drawing.Font("宋体", 18F);
-            this.textBox2.Location = new System.Drawing.Point(12, 122);
+            this.textBox2.Location = new System.Drawing.Point(17, 188);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(5);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(441, 48);
+            this.textBox2.Size = new System.Drawing.Size(812, 48);
             this.textBox2.TabIndex = 1;
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("宋体", 18F);
-            this.button1.Location = new System.Drawing.Point(475, 65);
+            this.button1.Location = new System.Drawing.Point(695, 22);
+            this.button1.Margin = new System.Windows.Forms.Padding(5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(147, 48);
+            this.button1.Size = new System.Drawing.Size(134, 62);
             this.button1.TabIndex = 2;
             this.button1.Text = "发送";
             this.button1.UseVisualStyleBackColor = true;
@@ -69,22 +80,13 @@
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("宋体", 18F);
-            this.textBox1.Location = new System.Drawing.Point(12, 65);
+            this.textBox1.Location = new System.Drawing.Point(17, 124);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(5);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(441, 48);
+            this.textBox1.Size = new System.Drawing.Size(812, 52);
             this.textBox1.TabIndex = 0;
             this.textBox1.Text = "N           0.00 g  \\\\";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("宋体", 28F);
-            this.label1.Location = new System.Drawing.Point(106, 24);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(131, 38);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "label1";
             // 
             // timer1
             // 
@@ -95,9 +97,10 @@
             // textBoxCom
             // 
             this.textBoxCom.Font = new System.Drawing.Font("宋体", 18F);
-            this.textBoxCom.Location = new System.Drawing.Point(99, 188);
+            this.textBoxCom.Location = new System.Drawing.Point(264, 16);
+            this.textBoxCom.Margin = new System.Windows.Forms.Padding(5);
             this.textBoxCom.Name = "textBoxCom";
-            this.textBoxCom.Size = new System.Drawing.Size(100, 35);
+            this.textBoxCom.Size = new System.Drawing.Size(196, 35);
             this.textBoxCom.TabIndex = 4;
             this.textBoxCom.TextChanged += new System.EventHandler(this.textBoxCom_TextChanged);
             // 
@@ -105,23 +108,139 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("宋体", 18F);
-            this.label2.Location = new System.Drawing.Point(9, 191);
+            this.label2.Location = new System.Drawing.Point(17, 21);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(106, 24);
+            this.label2.Size = new System.Drawing.Size(118, 24);
             this.label2.TabIndex = 5;
-            this.label2.Text = "串口号：";
+            this.label2.Text = "串口号1：";
+            // 
+            // serialPort2
+            // 
+            this.serialPort2.DataBits = 7;
+            this.serialPort2.Parity = System.IO.Ports.Parity.Odd;
+            this.serialPort2.PortName = "5";
+            this.serialPort2.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort2_DataReceived);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("宋体", 18F);
+            this.textBox3.Location = new System.Drawing.Point(256, 265);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(5);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(196, 35);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("宋体", 18F);
+            this.label5.Location = new System.Drawing.Point(9, 269);
+            this.label5.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(118, 24);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "串口号2：";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("宋体", 18F);
+            this.textBox4.Location = new System.Drawing.Point(17, 341);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(5);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(812, 52);
+            this.textBox4.TabIndex = 10;
+            this.textBox4.Text = "N           0.00 g  \\\\";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("宋体", 18F);
+            this.textBox5.Location = new System.Drawing.Point(17, 399);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(5);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(812, 48);
+            this.textBox5.TabIndex = 11;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("宋体", 18F);
+            this.button2.Location = new System.Drawing.Point(695, 251);
+            this.button2.Margin = new System.Windows.Forms.Padding(5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(134, 62);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "发送";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 18F);
+            this.label1.Location = new System.Drawing.Point(489, 269);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 24);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "文本路径：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 18F);
+            this.label3.Location = new System.Drawing.Point(489, 33);
+            this.label3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(130, 24);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "文本路径：";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(610, 22);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 53);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "label4";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label6.Location = new System.Drawing.Point(610, 258);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(62, 53);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "sd";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label6.UseMnemonic = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(639, 248);
-            this.Controls.Add(this.textBoxCom);
+            this.ClientSize = new System.Drawing.Size(841, 487);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBoxCom);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label2);
+            this.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(5);
             this.Name = "Form1";
             this.Text = "Form1";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
@@ -137,10 +256,19 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.TextBox textBoxCom;
         private System.Windows.Forms.Label label2;
+        private System.IO.Ports.SerialPort serialPort2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- appp/Form1.cs
+++ appp/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace appp
@@ -13,12 +14,14 @@ namespace appp
     public partial class Form1 : Form
     {
         string value_path = "C:\\ak.txt";
+        string value_path2 = "C:\\bk.txt";
         public Form1()
         {
             InitializeComponent();
         }
         bool start = false;
-        string value=string.Empty;
+        string value = string.Empty;
+        string value1 = string.Empty;
         private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
         {
             Byte[] InputBuf = new Byte[128];
@@ -48,13 +51,78 @@ namespace appp
             }
         }
 
+
+
+        private void serialPort2_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+
+
+
+            Byte[] InputBuf = new Byte[128];
+            try
+            {
+                System.Threading.Thread.Sleep(30);
+                if (serialPort2.IsOpen)
+                {
+                    serialPort2.Read(InputBuf, 0, serialPort2.BytesToRead);                                //读取缓冲区的数据直到“}”即0x7D为结束符  
+                                                                                                           //InputBuf = UnicodeEncoding.Default.GetBytes(strRD);             //将得到的数据转换成byte的格式  
+                    ASCIIEncoding encoding = new ASCIIEncoding();
+                    string str = encoding.GetString(InputBuf);
+                    str = str.Replace("\0", "");
+                    if (str.StartsWith("N") && str.Length == 22)
+                    {
+                        str = str.Substring(6, 10);
+                        str = str.Replace("+", "");
+                        str = str.Replace(" ", "");
+                        value1 = str;
+                    }
+                }
+
+            }
+            catch (TimeoutException ex)         //超时处理  
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
-            serialPort1.Write(textBox1.Text);
+            if (serialPort1.IsOpen)
+                serialPort1.Write(textBox1.Text);
+        }
+        private void Thread1()
+        {
+            string str;
+            str = Ini.Read("config", "com1");
+            try
+            {
+
+                if (str != "null")
+                {
+                    textBox3.Text = str;
+                    serialPort2.PortName = "COM" + Ini.Read("config", "com1");
+                    serialPort2.Open();
+
+                }
+                else
+                {
+                    MessageBox.Show("串口未配置！");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(string.Format("COM{0}打开失败！", str));
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            label4.Text = value_path;
+            label6.Text = value_path2;
+
+            // Thread thread1 = new Thread(new ThreadStart(Thread1));
+            //thread1.Start();
+            Thread1();
             open();
             start = true;
             if (!File.Exists(value_path))
@@ -67,6 +135,17 @@ namespace appp
                 //清空缓冲区、关闭流
                 fs.Flush();
                 fs.Close();
+            }
+            if (!File.Exists(value_path2))
+            {
+                FileStream fs1 = new FileStream(value_path2, FileMode.Create, FileAccess.Write, FileShare.ReadWrite);
+                //获得字节数组
+                byte[] data1 = Encoding.Default.GetBytes("0.00");
+                //开始写入
+                fs1.Write(data1, 0, data1.Length);
+                //清空缓冲区、关闭流
+                fs1.Flush();
+                fs1.Close();
             }
             AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
         }
@@ -106,7 +185,7 @@ namespace appp
                 StreamReader sr = new StreamReader(value_path, Encoding.Default);
                 String line;
                 line = sr.ReadLine();
-                label1.Text = line;
+                //label1.Text = line;
                 sr.Close();
 
                 if (line != value && value.Length > 0)
@@ -121,12 +200,47 @@ namespace appp
                     fs.Flush();
                     fs.Close();
                 }
+
+
+                StreamReader sr1 = new StreamReader(value_path2, Encoding.Default);
+                String line1;
+                line1 = sr1.ReadLine();
+                //label1.Text = line;
+                sr1.Close();
+
+                if (line1 != value1 && value1.Length > 0)
+                {
+                    textBox5.Text = value1;
+                    FileStream fs1 = new FileStream(value_path2, FileMode.Open, FileAccess.Write, FileShare.ReadWrite);
+                    //获得字节数组
+                    byte[] data2 = System.Text.Encoding.Default.GetBytes(value1 + "\0\0\0\0\0\0\0\0\0\0");
+                    //开始写入
+                    fs1.Write(data2, 0, data2.Length);
+                    //清空缓冲区、关闭流
+                    fs1.Flush();
+                    fs1.Close();
+                }
             }
             catch
             {
             }
         }
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            if (start)
+            {
+                string str = textBox3.Text.Replace(" ", "");
+                Ini.Write("config", "com1", str);
 
+                if (serialPort2.IsOpen)
+                {
+                    serialPort2.Close();
+                }
+
+                //重新打开
+                Thread1();
+            }
+        }
         private void textBoxCom_TextChanged(object sender, EventArgs e)
         {
             if (start)
@@ -150,6 +264,16 @@ namespace appp
             {
                 serialPort1.Close();
             }
+            if (serialPort2.IsOpen)
+            {
+                serialPort2.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (serialPort2.IsOpen)
+                serialPort2.Write(textBox4.Text);
         }
     }
 }
